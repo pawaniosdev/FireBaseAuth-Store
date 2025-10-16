4d005810bb4ad81d4bf80d9a578cf5c918bd615d
pawaniosdev <pawanarsd@gmail.com>
2025-10-17 01:21:46 +0530

    Initial Commit

---
+++ FirebaseAuth&Store/FirebaseAuth_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuth_StoreApp.swift
+//  FirebaseAuth&Store
+//
+//  Created by Pawan kumar Verma on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuth_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
